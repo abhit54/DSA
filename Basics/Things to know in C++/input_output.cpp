eadae1827eecd89ead75742afd34ccb73443d0d2
abhit54 <124334323+abhit54@users.noreply.github.com>
2023-02-03 11:40:40 +0530

    Create input_output.cpp

---
+++ Basics/Things to know in C++/input_output.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+//It includes all the libraries
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    cout<<"The value of x:"<<x<<" and y:"<<y<<"\n";
+    string s1,s2;
+    cin>>s1;
+    cout<<"Input for string is "<<s1<<endl;
+    getline(cin,s2);
+    cout<<"Input is "<<s2<<endl;
+    /*
+    Sample OUTPUT:
+    10 20
+    The value of x:10 and y:20
+    Thavishi Abhinaya JNTUHCEH B.Tech
+    Input for string is Thavishi
+    Input is  Abhinaya JNTUHCEH B.Tech
+    */
+    return 0;
+}
