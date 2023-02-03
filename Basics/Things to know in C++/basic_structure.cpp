5a7cf9af0fb8923f6d215b3cdf5f6f0c6fce21ee
abhit54 <124334323+abhit54@users.noreply.github.com>
2023-02-03 11:39:51 +0530

    Create basic_structure.cpp

---
+++ Basics/Things to know in C++/basic_structure.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+using namespace std;
+//Uses std for all the functions so, no need of writing it explicitly again.
+int main()
+{
+    /*std::cout<<"Hey Abhinaya\n"<<"Good Morning"<<std::endl;
+    std::cout<<"Hey Abhinaya";*/
+    cout<<"Hey Abhinaya\n"<<"Good Morning"<<endl;
+    // we can use endl or \n to goto next line.
+    cout<<"Hey Abhinaya";
+    return 0;
+}
